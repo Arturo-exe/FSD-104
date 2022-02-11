f4a60ab91cd34a3779ce718e30541f809b19cfc9
Arturo <99296330+Arturo-exe@users.noreply.github.com>
2022-02-11 10:37:15 -0800

    Add files via upload

---
+++ scripts/register.js
@@ -0,0 +1,35 @@
+let salon={
+    name:"Pet Salon",
+    address:{
+        steet:"Av.University",
+        ZipCode:"22664",
+        numer:"262-W",
+    },
+    hours:{
+        open:"9:00 am",
+        close:"8:00 pm"
+    },
+    pets:[]
+
+}
+
+
+// function displayPetNames(){
+
+//   salon.pets.forEach((element)=> {
+//     console.log(element.name);
+//   });
+
+//  alert(`They are ${salon.pets.length} on the line`)
+// }
+
+// displayPetNames();
+
+
+const register = () => {
+    name =document.getElementById("petName");
+};
+
+
+
+
